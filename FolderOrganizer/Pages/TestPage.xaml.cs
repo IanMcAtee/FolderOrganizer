e514135c5bae2e54a382f04a2719974a890d60f0
Ian McAtee <ianmcatee@protonmail.com>
2025-01-28 21:53:21 -0700

    Incremental Progress

--- FolderOrganizer/Pages/TestPage.xaml.cs
+++ FolderOrganizer/Pages/TestPage.xaml.cs
@@ -6,7 +6,9 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,9 +25,69 @@ namespace FolderOrganizer.Pages
     /// </summary>
     public sealed partial class TestPage : Page
     {
+        internal List<DefaultCategoryViewModel> DefaultCategoryListViewItems = new List<DefaultCategoryViewModel>();
         public TestPage()
         {
+            
+            
             this.InitializeComponent();
+
+            foreach (CategoryAndFileTypes caft in CommonCategoryToFileTypeMappings.CategoryAndFileTypesList)
+            {
+                DefaultCategoryViewModel viewModel = new DefaultCategoryViewModel();
+                viewModel.Caft = caft;
+                DefaultCategoryListViewItems.Add(viewModel);
+            }
+            defaultCategoriesListView.ItemsSource = DefaultCategoryListViewItems;
         }
+
+        private void DefaultCategoryExpand_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button expandButton = (Button)sender;
+            Panel? collapsiblePanel = XamlHelper.GetFirstSiblingOfType<Panel>(expandButton);
+            if (collapsiblePanel != null)
+            {
+                collapsiblePanel.Visibility = Visibility.Visible;
+                expandButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void DefaultCategoryCollapse_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button collapseButton = (Button)sender;
+            Panel parentPanel = (Panel)collapseButton.Parent;
+            parentPanel.Visibility = Visibility.Collapsed;
+            Button? expandButton = XamlHelper.GetFirstSiblingOfType<Button>(parentPanel);
+            if (expandButton != null)
+            {
+                expandButton.Visibility = Visibility.Visible;
+                
+            }
+        }
+    }
+
+    
+
+    internal class DefaultCategoryViewModel
+    {
+        public CategoryAndFileTypes? Caft { get; set; }
+        public ToggleSwitch? CategorySelectToggleSwitch { get; set; }
+        public TextBox? FileTypesTextBox { get; set; }
+        public Button? ApplyFileTypesButton { get; set; }
+        public ToggleSwitch? UseCustomFileTypesToggleSwitch { get; set; }
+
+        public void Expand_OnClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Expand Clicked");
+            
+        }
+
+        public void UseCustomFileTypes_Toggle(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = (ToggleSwitch)sender;
+        
+        }
+
+        /////I THINK AN ONLOADED METHOD TO POPULATE PROPERTIES IS THE WAY TO GO
     }
 }
